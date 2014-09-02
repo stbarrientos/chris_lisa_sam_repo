2dbcbff72a2f9fabedf4a334d9d58cbd2dbcf965
stbarrientos <stbarrientos@gmail.com>
2014-09-02 08:56:59 -0400

    final changes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,20 +1,36 @@
 class BlogsController < ApplicationController
 
-	before_action :authenticate!, only: [:new]
+	before_action :authenticate_user!, only: [:new]
 
 	def index
+		@blogs = Blog.all
 	end
 
 	def gallery
+		@blogs = Blog.all
 	end
 
 	def show
+		@blog = Blog.find(params[:id])
 	end
 
 	def new
+		@blog = Blog.new
 	end
 
 	def create
+		@blog = Blog.new(blog_params)
+		if @blog.save
+			redirect_to @blog
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def blog_params
+		params.require(:blog).permit(:title,:body)
 	end
 
 
